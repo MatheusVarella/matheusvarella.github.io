ca49867932c881d9616fdbfe02b431599758c486
MatheusVarella <varellamat@gmail.com>
2021-07-11 21:49:57 -0300

    Corrected slice

--- pages/Ponte Produtor/scripts/button.js
+++ pages/Ponte Produtor/scripts/button.js
@@ -1,3 +1,14 @@
-$( ".button-container" ).click(function() {
-  $( ".content" ).slideToggle( "slow" );
+let button = document.getElementsByClassName("button-container");
+
+$( button ).click(function() {
+  $( content ).slideToggle( "slow" );
+});
+
+button.addEventListener("click", () => {
+  let content = document.getElementsByClassName("content");
+  let i = document.getElementsByClassName("fa-arrow-down");
+  $( button ).click(function() {
+    $( content ).slideToggle( "slow" );
+    $( fa-arrow-down ).toggle();
+  });
 });
